233d8da864a39c6cbe6a45263724a0b8942d9d6a
esepgit <150302936+esepgit@users.noreply.github.com>
2024-04-03 18:57:07 -0300

    working in movil

--- modulo2/js-dia3/assets/js/app.js
+++ modulo2/js-dia3/assets/js/app.js
@@ -1,3 +1,19 @@
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready(function () {
+  $("#enviarCorreo").click(function () {
+    alert("El correo fue enviado correctamente...");
+  });
+
+  $("u").dblclick(function () {
+    $(this).css({
+      color: "red"
+    });
+  });
+
+ $(".recipe-title").click(function () {
+    $(".recipe-text").toggle();
+ })
+});
